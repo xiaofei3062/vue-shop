340c9497018995e3b3086c3948144f0de8a5b443
1592826708@qq.com <2165373shen>
2020-01-13 15:14:16 +0800

    优化分类逻辑

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,23 @@
 import Vue from "vue";
 import Router from "vue-router";
 
+// 正常路由加载,会将js跟css合并到一个文件
+import Home from "../views/home/Home";
+import Category from "../views/category/Category";
+import Cart from "../views/cart/Cart";
+import Profile from "../views/profile/Profile";
+import Login from "../views/login/Login";
+import Register from "../views/register/Register";
+import ProductDetail from "../views/detail/ProductDetail";
+
 // 使用箭头函数import引入,可以实现路由懒加载
-const Home = () => import("../views/home/Home");
-const Category = () => import("../views/category/Category");
-const Cart = () => import("../views/cart/Cart");
-const Profile = () => import("../views/profile/Profile");
-const Login = () => import("../views/login/Login");
-const Register = () => import("../views/register/Register");
-const ProductDetail = () => import("../views/detail/ProductDetail");
+// const Home = () => import("../views/home/Home");
+// const Category = () => import("../views/category/Category");
+// const Cart = () => import("../views/cart/Cart");
+// const Profile = () => import("../views/profile/Profile");
+// const Login = () => import("../views/login/Login");
+// const Register = () => import("../views/register/Register");
+// const ProductDetail = () => import("../views/detail/ProductDetail");
 
 Vue.use(Router);
 
--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -13,94 +13,94 @@
 </template>
 
 <script>
-  import SlideBar from "./children/SlideBar";
-  import NavBar from "@/components/common/navbar/NavBar";
-  import { getCategory, getCategoryDetail, getSubcategory } from "@/network/category";
-  import Subcategory from "./children/Subcategory";
-  import Scroll from "@/components/common/scroll/Scroll";
-  import TabControl from "@/components/content/tabControl/TabControl";
-  import GoodsList from "@/components/content/goods/GoodsList";
+import SlideBar from "./children/SlideBar";
+import NavBar from "@/components/common/navbar/NavBar";
+import { getCategory, getCategoryDetail, getSubcategory } from "@/network/category";
+import Subcategory from "./children/Subcategory";
+import Scroll from "@/components/common/scroll/Scroll";
+import TabControl from "@/components/content/tabControl/TabControl";
+import GoodsList from "@/components/content/goods/GoodsList";
 
-  export default {
-    name: "Category",
-    data() {
-      return {
-        // 分类内容数据
-        subcategoryList: [],
-        categoryDetailList: [],
-        // 分类侧边栏数据
-        categoryList: [],
-        titleList: ["流行", "新款", "精选"],
-        currentIndex: 0,
-        // 存储key
-        keyList: []
-      };
+export default {
+  name: "Category",
+  data() {
+    return {
+      // 分类内容数据
+      subcategoryList: [],
+      categoryDetailList: [],
+      // 分类侧边栏数据
+      categoryList: [],
+      titleList: ["流行", "新款", "精选"],
+      currentIndex: 0,
+      // 存储key
+      keyList: []
+    };
+  },
+  components: { GoodsList, TabControl, Scroll, Subcategory, NavBar, SlideBar },
+  methods: {
+    slideBarItemClick({ maitKey, index }) {
+      this.currentIndex = index;
+      // 每次切换分类初始化tabControl的下标
+      this.$refs.tabControl.curIndex = 0;
+      // 请求对应的推荐列表
+      this.getCategoryDetail(this.categoryList[this.currentIndex].miniWallkey, "pop");
+      // 如果keyList中存在maitKey证明数据已经请求过了
+      if (this.keyList.includes(maitKey)) return this.$store.commit("setLoading", false);
+      this.$store.commit("setLoading", true);
+      // 如果不存在请求数据并且存储key
+      this.keyList[index] = maitKey;
+      this.getSubcategory(this.categoryList[this.currentIndex].maitKey, index);
     },
-    components: { GoodsList, TabControl, Scroll, Subcategory, NavBar, SlideBar },
-    methods: {
-      slideBarItemClick({ maitKey, index }) {
-        this.currentIndex = index;
-        // 每次切换分类初始化tabControl的下标
-        this.$refs.tabControl.curIndex = 0;
-        // 请求对应的推荐列表
-        this.getCategoryDetail(this.categoryList[this.currentIndex].miniWallkey, "pop");
-        // 如果keyList中存在maitKey证明数据已经请求过了
-        if (this.keyList.includes(maitKey)) return this.$store.commit("setLoading", false);
-        this.$store.commit("setLoading", true);
-        // 如果不存在请求数据并且存储key
-        this.keyList.push(maitKey);
-        this.getSubcategory(this.categoryList[this.currentIndex].maitKey);
-      },
-      getCategory() {
-        getCategory().then(res => {
-          // console.log(res.data.category.list);
-          this.categoryList = res.data.category.list;
-          this.$nextTick(() => {
-            this.getSubcategory(this.categoryList[0].maitKey);
-            this.getCategoryDetail(this.categoryList[0].miniWallkey, "pop");
-            // 初始化push第一个key进去
-            this.keyList.push(this.categoryList[0].maitKey);
-          });
+    getCategory() {
+      getCategory().then(res => {
+        // console.log(res.data.category.list);
+        this.categoryList = res.data.category.list;
+        this.$nextTick(() => {
+          this.getSubcategory(this.categoryList[0].maitKey, 0);
+          this.getCategoryDetail(this.categoryList[0].miniWallkey, "pop");
+          // 初始化push第一个key进去
+          this.keyList.push(this.categoryList[0].maitKey);
         });
-      },
-      getSubcategory(key) {
-        getSubcategory(key).then(res => {
-          // console.log(res.data.list);
-          this.subcategoryList.push(res.data.list);
-        });
-      },
-      getCategoryDetail(key, type) {
-        getCategoryDetail(key, type).then(res => {
-          // console.log(res);
-          this.categoryDetailList = res;
-        });
-      },
-      tabClick(index) {
-        this.$store.commit("setLoading", true);
-        const typeList = ["pop", "new", "sell"];
-        // 切换类型数据
-        this.getCategoryDetail(this.categoryList[this.currentIndex].miniWallkey, typeList[index]);
-      }
+      });
     },
-    created() {
-      this.getCategory();
+    getSubcategory(key, index) {
+      getSubcategory(key).then(res => {
+        // console.log(res.data.list);
+        this.subcategoryList[index] = res.data.list;
+      });
+    },
+    getCategoryDetail(key, type) {
+      getCategoryDetail(key, type).then(res => {
+        // console.log(res);
+        this.categoryDetailList = res;
+      });
+    },
+    tabClick(index) {
+      this.$store.commit("setLoading", true);
+      const typeList = ["pop", "new", "sell"];
+      // 切换类型数据
+      this.getCategoryDetail(this.categoryList[this.currentIndex].miniWallkey, typeList[index]);
     }
-  };
+  },
+  created() {
+    this.getCategory();
+  }
+};
 </script>
 
 <style scoped>
-  .category-nav-bar {
-    font-weight: 600;
-    color: white;
-    background-color: #ff8198;
-  }
+.category-nav-bar {
+  font-weight: 600;
+  color: white;
+  background-color: #ff8198;
+}
 
-  .scroll-height {
-    position: fixed;
-    top: 44px;
-    right: 0;
-    bottom: 50px;
-    left: 100px;
-    overflow: hidden;
-  }
+.scroll-height {
+  position: fixed;
+  top: 44px;
+  right: 0;
+  bottom: 50px;
+  left: 100px;
+  overflow: hidden;
+}
 </style>
